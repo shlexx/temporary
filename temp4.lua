8a755452fa624f65d06cbfcf53b64bd7aa30f889
shlexx <ginirbi@gmail.com>
2024-09-06 16:03:32 +0300

    Update temp4.lua

--- temp4.lua
+++ temp4.lua
@@ -16,6 +16,67 @@ Section:AddTextbox({
 		game.ReplicatedStorage.Remotes.UpdateHighestNumber:FireServer(Value)
 	end	  
 })
+local Section2 = Tab:AddSection({
+	Name = "Obby"
+})
+Section2:AddButton({
+	Name = "Instant Win",
+	Callback = function()
+      		local hrp = game.Players.LocalPlayer.Character.HumanoidRootPart
+            hrp.CFrame = CFrame.new(533, 228, -257)
+            wait(.5)
+            hrp.CFrame = CFrame.new(554, 291, -162)
+  	end    
+})
+Section2:AddButton({
+	Name = "Delete Barrier",
+	Callback = function()
+      		workspace.Obby.Barrier:Destroy()
+  	end    
+})
+local Section3 = Tab:AddSection({
+	Name = "Color Picker"
+})
+Section3:AddButton({
+	Name = "Anti-Void",
+	Callback = function()
+      		workspace.ColorMemory.FallBrick:Destroy()
+  	end    
+})
+local Section4 = Tab:AddSection({
+	Name = "Glass Bridge"
+})
+Section4:AddButton({
+	Name = "Delete Barrier",
+	Callback = function()
+      		workspace.GlassBridge.Barrier:Destroy()
+  	end    
+})
+Section4:AddButton({
+	Name = "Instant Win",
+	Callback = function()
+      		if game.Players.LocalPlayer.Team == game.Teams.Boys then
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.GlassBridge.BoysGlassBridge.WinDetector.CFrame
+            else
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.GlassBridge.GirlsGlassBridge.WinDetector.CFrame
+            end
+  	end    
+})
+local Section5 = Tab:AddSection({
+	Name = "Red Light Green Light"
+})
+Section5:AddButton({
+	Name = "Delete Barrier",
+	Callback = function()
+      		workspace.RedLightGreenLight.Barrier:Destroy()
+  	end    
+})
+Section5:AddButton({
+	Name = "Instant Win",
+	Callback = function()
+      		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.RedLightGreenLight.WinDetector.CFrame
+  	end    
+})
 local Tab2 = Window:MakeTab({
 	Name = "Misc",
 	Icon = "rbxassetid://4483345998",
@@ -46,25 +107,37 @@ Tab2:AddTextbox({
 	end	  
 })
 Tab2:AddButton({
-	Name = "Become A Boy",
+	Name = "Become A Boy (LOBBY)",
 	Callback = function()
       		game.ReplicatedStorage.Remotes.SetGender:FireServer("L")
   	end    
 })
 Tab2:AddButton({
-	Name = "Become A Girl",
+	Name = "Become A Girl (LOBBY)",
 	Callback = function()
       		game.ReplicatedStorage.Remotes.SetGender:FireServer("R")
   	end    
 })
 Tab2:AddButton({
-	Name = "Switch Gender",
+	Name = "Switch Gender (LOBBY)",
 	Callback = function()
       		if game.Players.LocalPlayer.Team == game.Teams.Boys then
                 game.ReplicatedStorage.Remotes.SetGender:FireServer("R")
             else
                 game.ReplicatedStorage.Remotes.SetGender:FireServer("L")
             end
+  	end    
+})
+Tab2:AddButton({
+	Name = "Baseplate",
+	Callback = function()
+      		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(119, -8, -224)
+  	end    
+})
+Tab2:AddButton({
+	Name = "Infinite Yield",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
   	end    
 })
 OrionLib:Init()
