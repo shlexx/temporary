ec5251c98b48d4396d2fb7cda91604e9a4011136
shlexx <ginirbi@gmail.com>
2024-11-08 22:03:33 +0200

    Update sagc.lua

--- sagc.lua
+++ sagc.lua
@@ -27,3 +27,59 @@ local gravcoil = Instance.new("Tool", game.Players.LocalPlayer.Backpack)
 		gravcoil.Unequipped:Connect(function()
 			workspace.Gravity = 196.2
 		end)
+local cam = workspace.CurrentCamera
+		local equipped = false
+		local plr = game.Players.LocalPlayer
+		local hrp = plr.Character:FindFirstChild("HumanoidRootPart")
+		local jetpack = Instance.new("Tool", plr.Backpack)
+		jetpack.Name = "Jetpack"
+		jetpack.TextureId = "rbxassetid://31310254"
+		jetpack.RequiresHandle = false
+		jetpack.CanBeDropped = false
+		
+		jetpack.Equipped:Connect(function()
+			equipped = true
+		end)
+		
+		jetpack.Unequipped:Connect(function()
+			equipped = false
+		end)
+		
+		jetpack.Activated:Connect(function()
+			if equipped then
+				local bodyGyro = Instance.new("BodyGyro", hrp)
+				local force = Instance.new("BodyVelocity", hrp)
+				bodyGyro.cframe = cam.CoordinateFrame
+				force.velocity = Vector3.new(0,30,0)
+			end
+		end)
+		
+		jetpack.Deactivated:Connect(function()
+			hrp:FindFirstChildOfClass("BodyGyro"):Destroy()
+			hrp:FindFirstChildOfClass("BodyVelocity"):Destroy()
+		end)
+local grappling = Instance.new("Tool", game.Players.LocalPlayer.Backpack)
+		grappling.Name = "GrapplingHook"
+		grappling.CanBeDropped = false
+		grappling.RequiresHandle = false
+		grappling.TextureId = "rbxassetid://30327988"
+		local mouse = game.Players.LocalPlayer:GetMouse()
+		local equipped = false
+		local tweenSpeed = 1
+		local TweenService = game:GetService("TweenService")
+
+		grappling.Equipped:Connect(function()
+			equipped = true
+		end)
+
+		grappling.Unequipped:Connect(function()
+			equipped = false
+		end)
+
+		grappling.Activated:Connect(function()
+			if equipped == true then
+				if mouse.Target then
+					TweenService:Create(game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart"), TweenInfo.new(tweenSpeed, Enum.EasingStyle.Linear, Enum.EasingDirection.Out), {CFrame = mouse.Hit + Vector3.new(0, 2, 0)}):Play()
+				end
+			else end
+		end)
