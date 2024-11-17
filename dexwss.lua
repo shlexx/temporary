4710ac21f6fd20a3824134db28d735736b97700f
shlexx <ginirbi@gmail.com>
2024-11-17 16:56:16 +0200

    Update dexwss.lua

--- dexwss.lua
+++ dexwss.lua
@@ -1,113 +1,6 @@
 -- https://github.com/LorekeeperZinnia/Dex
 
-function ssdecomp(obj)
-local source
-if obj:IsA("Script") then
-if obj.Name == "AutomaticUpdating" then
-source = [[local Tool = script.Parent.Parent;
-
-function IsVersionOutdated(Version)
-	-- Returns whether the given version of Building Tools is out of date
-
-	-- Check most recent version number
-	local AssetInfo = Game:GetService('MarketplaceService'):GetProductInfo(142785488, Enum.InfoType.Asset);
-	local LatestMajorVersion, LatestMinorVersion, LatestPatchVersion = AssetInfo.Description:match '%[Version: ([0-9]+)%.([0-9]+)%.([0-9]+)%]';
-	local CurrentMajorVersion, CurrentMinorVersion, CurrentPatchVersion = Version:match '([0-9]+)%.([0-9]+)%.([0-9]+)';
-
-	-- Convert version data into numbers
-	local LatestMajorVersion, LatestMinorVersion, LatestPatchVersion =
-		tonumber(LatestMajorVersion), tonumber(LatestMinorVersion), tonumber(LatestPatchVersion);
-	local CurrentMajorVersion, CurrentMinorVersion, CurrentPatchVersion =
-		tonumber(CurrentMajorVersion), tonumber(CurrentMinorVersion), tonumber(CurrentPatchVersion);
-
-	-- Determine whether current version is outdated
-	if LatestMajorVersion > CurrentMajorVersion then
-		return true;
-	elseif LatestMajorVersion == CurrentMajorVersion then
-		if LatestMinorVersion > CurrentMinorVersion then
-			return true;
-		elseif LatestMinorVersion == CurrentMinorVersion then
-			return LatestPatchVersion > CurrentPatchVersion;
-		end;
-	end;
-
-	-- Return an up-to-date status if not oudated
-	return false;
-
-end;
-
--- Ensure tool mode is enabled, auto-updating is enabled, and version is outdated
-if not (Tool:IsA 'Tool' and Tool.AutoUpdate.Value and IsVersionOutdated(Tool.Version.Value)) then
-	return;
-end;
-
--- Use module to insert latest tool
-local GetLatestTool = require(580330877);
-if not GetLatestTool then
-	return;
-end;
-
--- Get latest copy of tool
-local NewTool = GetLatestTool();
-if NewTool then
-
-	-- Prevent update attempt loops since fetched version is now cached
-	NewTool.AutoUpdate.Value = false;
-
-	-- Cancel replacing current tool if fetched version is the same
-	if NewTool.Version.Value == Tool.Version.Value then
-		return;
-	end;
-
-	-- Detach update script from tool and save old tool parent
-	script.Parent = nil;
-	local ToolParent = Tool.Parent;
-
-	-- Remove current tool (delayed to prevent parenting conflicts)
-	wait(0.05);
-	Tool.Parent = nil;
-
-	-- Remove the tool again if anything attempts to reparent it
-	Tool.Changed:Connect(function (Property)
-		if Property == 'Parent' and Tool.Parent then
-			wait(0.05);
-			Tool.Parent = nil;
-		end;
-	end);
-
-	-- Add the new tool
-	NewTool.Parent = ToolParent;
-
-end;]]
-elseif obj.Name == "DescendantCounter" then
-source = [[local Count = script.Parent;
-local Tool = Count.Parent.Parent;
-
--- Exclude counting autoremoving items (thumbnail and autoupdating script)
-local AutoremovingItemsCount = 5 + 1;
-
--- Provide total count of all descendants
-Count.Value = #Tool:GetDescendants() - AutoremovingItemsCount;]]
-elseif obj.Name == "ServerEndpoint" then
-source = [[local ServerEndpoint = script.Parent;
-local SyncAPI = ServerEndpoint.Parent;
-local Tool = SyncAPI.Parent;
-
--- Start the server-side sync module
-SyncModule = require(SyncAPI:WaitForChild 'SyncModule');
-
--- Provide functionality to the server API endpoint instance
-ServerEndpoint.OnServerInvoke = function (Client, ...)
-	return SyncModule.PerformAction(Client, ...);
-end;]]
-elseif obj.Name == "PluginInitializer" then
-source = [[if plugin then
-    require(script.Parent)
-end]]
-end
-end
-return source
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexx/grxvuqe/main/premadedecompile.lua"))()
 
 --[[
 	New Dex
@@ -4319,8 +4212,10 @@ local function main()
 	local PreviousScr = nil
 
 	ScriptViewer.ViewScript = function(scr)
-		local success, source = pcall(ssdecomp or env.decompile or function() end, scr)
-		if not success or not source then source, PreviousScr = "-- DEX - Source failed to decompile", nil else PreviousScr = scr end
+		if scr.ClassName == "Script" then local success, source = pcall(_G.decompile or function() end, scr)
+		if not success or not source then source, PreviousScr = "-- DEX - Source failed to decompile", nil else PreviousScr = scr end else
+		local success, source = pcall(env.decompile or function() end, scr)
+		if not success or not source then source, PreviousScr = "-- DEX - Source failed to decompile", nil else PreviousScr = scr end end
 		codeFrame:SetText(source:gsub("\0", "\\0")) -- Fix stupid breaking script viewer
 		window:Show()
 	end
