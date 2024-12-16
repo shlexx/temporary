b0cf57de32514a45bf2bff2558ecdce459f2d39c
shlexx <ginirbi@gmail.com>
2024-12-16 16:40:48 +0200

    Create savechosen.lua

---
+++ savechosen.lua
@@ -0,0 +1,490 @@
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextBox = Instance.new("TextBox")
+local UICorner = Instance.new("UICorner")
+local TextButton = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local UICorner_3 = Instance.new("UICorner")
+local TextButton2 = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local TextButton3 = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+local TextButton5 = Instance.new("TextButton")
+local UICorner_6 = Instance.new("UICorner")
+local TextButton6 = Instance.new("TextButton")
+local UICorner_7 = Instance.new("UICorner")
+local TextButton7 = Instance.new("TextButton")
+local UICorner_8 = Instance.new("UICorner")
+local TextButton4 = Instance.new("TextButton")
+local UICorner_9 = Instance.new("UICorner")
+local TextButton8 = Instance.new("TextButton")
+local UICorner_10 = Instance.new("UICorner")
+
+ScreenGui.Parent = game.CoreGui.RobloxGui
+ScreenGui.ResetOnSpawn = false
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.175516218, 0, 0.241206035, 0)
+Frame.Size = UDim2.new(0, 350, 0, 260)
+
+TextBox.Parent = Frame
+TextBox.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+TextBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.BorderSizePixel = 0
+TextBox.Position = UDim2.new(0.034865547, 0, 0.188461781, 0)
+TextBox.Size = UDim2.new(0, 143, 0, 40)
+TextBox.Font = Enum.Font.FredokaOne
+TextBox.PlaceholderText = "enter command"
+TextBox.Text = ""
+TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextBox.TextScaled = true
+TextBox.TextSize = 14.000
+TextBox.TextWrapped = true
+
+UICorner.Parent = TextBox
+
+TextButton.Parent = Frame
+TextButton.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.BorderSizePixel = 0
+TextButton.Position = UDim2.new(0.034865547, 0, 0.384996742, 0)
+TextButton.Size = UDim2.new(0, 143, 0, 40)
+TextButton.Font = Enum.Font.FredokaOne
+TextButton.Text = "execute command"
+TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
+
+UICorner_2.Parent = TextButton
+
+UICorner_3.CornerRadius = UDim.new(0, 16)
+UICorner_3.Parent = Frame
+
+TextButton2.Name = "TextButton2"
+TextButton2.Parent = Frame
+TextButton2.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+TextButton2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton2.BorderSizePixel = 0
+TextButton2.Position = UDim2.new(0.034865547, 0, 0.581531584, 0)
+TextButton2.Size = UDim2.new(0, 143, 0, 40)
+TextButton2.Font = Enum.Font.FredokaOne
+TextButton2.Text = "save enlighten"
+TextButton2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton2.TextScaled = true
+TextButton2.TextSize = 14.000
+TextButton2.TextWrapped = true
+
+UICorner_4.Parent = TextButton2
+
+TextButton3.Name = "TextButton3"
+TextButton3.Parent = Frame
+TextButton3.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+TextButton3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton3.BorderSizePixel = 0
+TextButton3.Position = UDim2.new(0.034865547, 0, 0.778066576, 0)
+TextButton3.Size = UDim2.new(0, 143, 0, 40)
+TextButton3.Font = Enum.Font.FredokaOne
+TextButton3.Text = "save bkit"
+TextButton3.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton3.TextScaled = true
+TextButton3.TextSize = 14.000
+TextButton3.TextWrapped = true
+
+UICorner_5.Parent = TextButton3
+
+TextButton5.Name = "TextButton5"
+TextButton5.Parent = Frame
+TextButton5.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+TextButton5.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton5.BorderSizePixel = 0
+TextButton5.Position = UDim2.new(0.549151242, 0, 0.384996742, 0)
+TextButton5.Size = UDim2.new(0, 143, 0, 40)
+TextButton5.Font = Enum.Font.FredokaOne
+TextButton5.Text = "take out enlighten"
+TextButton5.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton5.TextScaled = true
+TextButton5.TextSize = 14.000
+TextButton5.TextWrapped = true
+
+UICorner_6.Parent = TextButton5
+
+TextButton6.Name = "TextButton6"
+TextButton6.Parent = Frame
+TextButton6.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+TextButton6.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton6.BorderSizePixel = 0
+TextButton6.Position = UDim2.new(0.549151242, 0, 0.581531584, 0)
+TextButton6.Size = UDim2.new(0, 143, 0, 40)
+TextButton6.Font = Enum.Font.FredokaOne
+TextButton6.Text = "take out bkit"
+TextButton6.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton6.TextScaled = true
+TextButton6.TextSize = 14.000
+TextButton6.TextWrapped = true
+
+UICorner_7.Parent = TextButton6
+
+TextButton7.Name = "TextButton7"
+TextButton7.Parent = Frame
+TextButton7.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+TextButton7.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton7.BorderSizePixel = 0
+TextButton7.Position = UDim2.new(0.549151242, 0, 0.778066576, 0)
+TextButton7.Size = UDim2.new(0, 143, 0, 40)
+TextButton7.Font = Enum.Font.FredokaOne
+TextButton7.Text = "take out others"
+TextButton7.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton7.TextScaled = true
+TextButton7.TextSize = 14.000
+TextButton7.TextWrapped = true
+
+UICorner_8.Parent = TextButton7
+
+TextButton4.Name = "TextButton4"
+TextButton4.Parent = Frame
+TextButton4.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+TextButton4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton4.BorderSizePixel = 0
+TextButton4.Position = UDim2.new(0.549151242, 0, 0.188461781, 0)
+TextButton4.Size = UDim2.new(0, 143, 0, 40)
+TextButton4.Font = Enum.Font.FredokaOne
+TextButton4.Text = "save others"
+TextButton4.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton4.TextScaled = true
+TextButton4.TextSize = 14.000
+TextButton4.TextWrapped = true
+
+UICorner_9.Parent = TextButton4
+
+TextButton8.Name = "TextButton8"
+TextButton8.Parent = Frame
+TextButton8.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+TextButton8.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton8.BorderSizePixel = 0
+TextButton8.Position = UDim2.new(0.034865547, 0, 0.0273044296, 0)
+TextButton8.Size = UDim2.new(0, 322, 0, 32)
+TextButton8.Font = Enum.Font.FredokaOne
+TextButton8.Text = "reset buttons"
+TextButton8.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton8.TextScaled = true
+TextButton8.TextSize = 14.000
+TextButton8.TextWrapped = true
+
+UICorner_10.Parent = TextButton8
+
+local UIS = game:GetService("UserInputService")
+local function dragify(Frame,boool)
+	local frametomove = Frame
+	local dragToggle,dragInput,dragStart,startPos
+	local dragSpeed = 0
+	local function updateInput(input)
+		local Delta = input.Position - dragStart
+		frametomove.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
+	end
+	Frame.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and UIS:GetFocusedTextBox() == nil then
+			dragToggle = true
+			dragStart = input.Position
+			startPos = frametomove.Position
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragToggle = false
+				end
+			end)	
+		end
+	end)
+	Frame.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			dragInput = input
+		end
+	end)
+	UIS.InputChanged:Connect(function(input)
+		if input == dragInput and dragToggle then
+			updateInput(input)
+		end
+	end)
+end
+dragify(Frame)
+local backpack = game.Players.LocalPlayer.Backpack
+local storage = game.CoreGui:FindFirstChild("Storage")
+if not storage then
+	storage = Instance.new("Folder",game.CoreGui)
+	storage.Name = "Storage"
+end
+TextButton.MouseButton1Click:Connect(function()
+	local command = TextBox.Text
+	if command:sub(0,1) ~= ";" then
+		command = ";" .. command
+	end
+	game:GetService("CoreGui").Storage["The Arkenstone"].Parent = backpack
+	wait(.1)
+	game.Players.LocalPlayer.Character.Humanoid:EquipTool(backpack["The Arkenstone"])
+	wait(.1)
+	game:GetService("TextChatService").ChatInputBarConfiguration.TargetTextChannel:SendAsync(command)
+	wait(.1)
+	game.Players.LocalPlayer.Character["The Arkenstone"].Parent = game:GetService("CoreGui").Storage
+end)
+TextButton2.MouseButton1Click:Connect(function()
+	backpack["The Arkenstone"].Parent = storage
+end)
+
+TextButton3.MouseButton1Click:Connect(function()
+	backpack.Build.Parent = storage
+	backpack.Delete.Parent = storage
+	backpack.Paint.Parent = storage
+	backpack.Shape.Parent = storage
+	backpack.Shovel.Parent = storage
+	backpack.Sign.Parent = storage
+end)
+TextButton4.MouseButton1Click:Connect(function()
+	for i,v in pairs(backpack:GetChildren()) do
+		if v.Name ~= "The Arkenstone" and v.Name ~= "Build" and v.Name ~= "Delete" and v.Name ~= "Paint" and v.Name ~= "Shape" and v.Name ~= "Shovel" and v.Name ~= "Sign" then
+			v.Parent = storage
+		end
+	end
+end)
+TextButton5.MouseButton1Click:Connect(function()
+	storage["The Arkenstone"].Parent = backpack
+end)
+TextButton6.MouseButton1Click:Connect(function()
+	storage.Build.Parent = backpack
+	storage.Delete.Parent = backpack
+	storage.Paint.Parent = backpack
+	storage.Shape.Parent = backpack
+	storage.Shovel.Parent = backpack
+	storage.Sign.Parent = backpack
+end)
+TextButton7.MouseButton1Click:Connect(function()
+	for i,v in pairs(storage:GetChildren()) do
+		if v.Name ~= "The Arkenstone" and v.Name ~= "Build" and v.Name ~= "Delete" and v.Name ~= "Paint" and v.Name ~= "Shape" and v.Name ~= "Shovel" and v.Name ~= "Sign" then
+			v.Parent = backpack
+		end
+	end
+end)
+TextButton8.MouseButton1Click:Connect(function()
+	TextBox:Destroy()
+	TextButton:Destroy()
+	TextButton2:Destroy()
+	TextButton3:Destroy()
+	TextButton4:Destroy()
+	TextButton5:Destroy()
+	TextButton6:Destroy()
+	TextButton7:Destroy()
+
+    local TextBox = Instance.new("TextBox")
+    local UICorner = Instance.new("UICorner")
+    local TextButton = Instance.new("TextButton")
+    local UICorner_2 = Instance.new("UICorner")
+    local TextButton2 = Instance.new("TextButton")
+    local UICorner_4 = Instance.new("UICorner")
+    local TextButton3 = Instance.new("TextButton")
+    local UICorner_5 = Instance.new("UICorner")
+    local TextButton5 = Instance.new("TextButton")
+    local UICorner_6 = Instance.new("UICorner")
+    local TextButton6 = Instance.new("TextButton")
+    local UICorner_7 = Instance.new("UICorner")
+    local TextButton7 = Instance.new("TextButton")
+    local UICorner_8 = Instance.new("UICorner")
+    local TextButton4 = Instance.new("TextButton")
+    local UICorner_9 = Instance.new("UICorner")
+    local TextButton8 = Instance.new("TextButton")
+    local UICorner_10 = Instance.new("UICorner")
+	
+	TextBox.Parent = Frame
+	TextBox.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+	TextBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TextBox.BorderSizePixel = 0
+	TextBox.Position = UDim2.new(0.034865547, 0, 0.188461781, 0)
+	TextBox.Size = UDim2.new(0, 143, 0, 40)
+	TextBox.Font = Enum.Font.FredokaOne
+	TextBox.PlaceholderText = "enter command"
+	TextBox.Text = ""
+	TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
+	TextBox.TextScaled = true
+	TextBox.TextSize = 14.000
+	TextBox.TextWrapped = true
+
+	UICorner.Parent = TextBox
+
+	TextButton.Parent = Frame
+	TextButton.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+	TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TextButton.BorderSizePixel = 0
+	TextButton.Position = UDim2.new(0.034865547, 0, 0.384996742, 0)
+	TextButton.Size = UDim2.new(0, 143, 0, 40)
+	TextButton.Font = Enum.Font.FredokaOne
+	TextButton.Text = "execute command"
+	TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+	TextButton.TextScaled = true
+	TextButton.TextSize = 14.000
+	TextButton.TextWrapped = true
+
+	UICorner_2.Parent = TextButton
+
+	TextButton2.Name = "TextButton2"
+	TextButton2.Parent = Frame
+	TextButton2.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+	TextButton2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TextButton2.BorderSizePixel = 0
+	TextButton2.Position = UDim2.new(0.034865547, 0, 0.581531584, 0)
+	TextButton2.Size = UDim2.new(0, 143, 0, 40)
+	TextButton2.Font = Enum.Font.FredokaOne
+	TextButton2.Text = "save enlighten"
+	TextButton2.TextColor3 = Color3.fromRGB(255, 255, 255)
+	TextButton2.TextScaled = true
+	TextButton2.TextSize = 14.000
+	TextButton2.TextWrapped = true
+
+	UICorner_4.Parent = TextButton2
+
+	TextButton3.Name = "TextButton3"
+	TextButton3.Parent = Frame
+	TextButton3.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+	TextButton3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TextButton3.BorderSizePixel = 0
+	TextButton3.Position = UDim2.new(0.034865547, 0, 0.778066576, 0)
+	TextButton3.Size = UDim2.new(0, 143, 0, 40)
+	TextButton3.Font = Enum.Font.FredokaOne
+	TextButton3.Text = "save bkit"
+	TextButton3.TextColor3 = Color3.fromRGB(255, 255, 255)
+	TextButton3.TextScaled = true
+	TextButton3.TextSize = 14.000
+	TextButton3.TextWrapped = true
+
+	UICorner_5.Parent = TextButton3
+
+	TextButton5.Name = "TextButton5"
+	TextButton5.Parent = Frame
+	TextButton5.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+	TextButton5.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TextButton5.BorderSizePixel = 0
+	TextButton5.Position = UDim2.new(0.549151242, 0, 0.384996742, 0)
+	TextButton5.Size = UDim2.new(0, 143, 0, 40)
+	TextButton5.Font = Enum.Font.FredokaOne
+	TextButton5.Text = "take out enlighten"
+	TextButton5.TextColor3 = Color3.fromRGB(255, 255, 255)
+	TextButton5.TextScaled = true
+	TextButton5.TextSize = 14.000
+	TextButton5.TextWrapped = true
+
+	UICorner_6.Parent = TextButton5
+
+	TextButton6.Name = "TextButton6"
+	TextButton6.Parent = Frame
+	TextButton6.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+	TextButton6.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TextButton6.BorderSizePixel = 0
+	TextButton6.Position = UDim2.new(0.549151242, 0, 0.581531584, 0)
+	TextButton6.Size = UDim2.new(0, 143, 0, 40)
+	TextButton6.Font = Enum.Font.FredokaOne
+	TextButton6.Text = "take out bkit"
+	TextButton6.TextColor3 = Color3.fromRGB(255, 255, 255)
+	TextButton6.TextScaled = true
+	TextButton6.TextSize = 14.000
+	TextButton6.TextWrapped = true
+
+	UICorner_7.Parent = TextButton6
+
+	TextButton7.Name = "TextButton7"
+	TextButton7.Parent = Frame
+	TextButton7.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+	TextButton7.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TextButton7.BorderSizePixel = 0
+	TextButton7.Position = UDim2.new(0.549151242, 0, 0.778066576, 0)
+	TextButton7.Size = UDim2.new(0, 143, 0, 40)
+	TextButton7.Font = Enum.Font.FredokaOne
+	TextButton7.Text = "take out others"
+	TextButton7.TextColor3 = Color3.fromRGB(255, 255, 255)
+	TextButton7.TextScaled = true
+	TextButton7.TextSize = 14.000
+	TextButton7.TextWrapped = true
+
+	UICorner_8.Parent = TextButton7
+
+	TextButton4.Name = "TextButton4"
+	TextButton4.Parent = Frame
+	TextButton4.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+	TextButton4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TextButton4.BorderSizePixel = 0
+	TextButton4.Position = UDim2.new(0.549151242, 0, 0.188461781, 0)
+	TextButton4.Size = UDim2.new(0, 143, 0, 40)
+	TextButton4.Font = Enum.Font.FredokaOne
+	TextButton4.Text = "save others"
+	TextButton4.TextColor3 = Color3.fromRGB(255, 255, 255)
+	TextButton4.TextScaled = true
+	TextButton4.TextSize = 14.000
+	TextButton4.TextWrapped = true
+
+	UICorner_9.Parent = TextButton4
+
+	TextButton8.Name = "TextButton8"
+	TextButton8.Parent = Frame
+	TextButton8.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+	TextButton8.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TextButton8.BorderSizePixel = 0
+	TextButton8.Position = UDim2.new(0.034865547, 0, 0.0273044296, 0)
+	TextButton8.Size = UDim2.new(0, 322, 0, 32)
+	TextButton8.Font = Enum.Font.FredokaOne
+	TextButton8.Text = "reset buttons"
+	TextButton8.TextColor3 = Color3.fromRGB(255, 255, 255)
+	TextButton8.TextScaled = true
+	TextButton8.TextSize = 14.000
+	TextButton8.TextWrapped = true
+
+	UICorner_10.Parent = TextButton8
+	
+	TextButton.MouseButton1Click:Connect(function()
+		local command = TextBox.Text
+		if command:sub(0,1) ~= ";" then
+			command = ";" .. command
+		end
+		game:GetService("CoreGui").Storage["The Arkenstone"].Parent = backpack
+		wait(.1)
+		game.Players.LocalPlayer.Character.Humanoid:EquipTool(backpack["The Arkenstone"])
+		wait(.1)
+		game:GetService("TextChatService").ChatInputBarConfiguration.TargetTextChannel:SendAsync(command)
+		wait(.1)
+		game.Players.LocalPlayer.Character["The Arkenstone"].Parent = game:GetService("CoreGui").Storage
+	end)
+	TextButton2.MouseButton1Click:Connect(function()
+		backpack["The Arkenstone"].Parent = storage
+	end)
+
+	TextButton3.MouseButton1Click:Connect(function()
+		backpack.Build.Parent = storage
+		backpack.Delete.Parent = storage
+		backpack.Paint.Parent = storage
+		backpack.Shape.Parent = storage
+		backpack.Shovel.Parent = storage
+		backpack.Sign.Parent = storage
+	end)
+	TextButton4.MouseButton1Click:Connect(function()
+		for i,v in pairs(backpack:GetChildren()) do
+			if v.Name ~= "The Arkenstone" and v.Name ~= "Build" and v.Name ~= "Delete" and v.Name ~= "Paint" and v.Name ~= "Shape" and v.Name ~= "Shovel" and v.Name ~= "Sign" then
+				v.Parent = storage
+			end
+		end
+	end)
+	TextButton5.MouseButton1Click:Connect(function()
+		storage["The Arkenstone"].Parent = backpack
+	end)
+	TextButton6.MouseButton1Click:Connect(function()
+		storage.Build.Parent = backpack
+		storage.Delete.Parent = backpack
+		storage.Paint.Parent = backpack
+		storage.Shape.Parent = backpack
+		storage.Shovel.Parent = backpack
+		storage.Sign.Parent = backpack
+	end)
+	TextButton7.MouseButton1Click:Connect(function()
+		for i,v in pairs(storage:GetChildren()) do
+			if v.Name ~= "The Arkenstone" and v.Name ~= "Build" and v.Name ~= "Delete" and v.Name ~= "Paint" and v.Name ~= "Shape" and v.Name ~= "Shovel" and v.Name ~= "Sign" then
+				v.Parent = backpack
+			end
+		end
+	end)
+end)
